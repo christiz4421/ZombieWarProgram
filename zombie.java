27a423eaa6865d987dc441d0ee3b021aa1c7a41c
saurot <saurot@csp.edu>
2026-02-16 15:34:18 -0600

    Add files via upload

---
+++ zombie.java
@@ -0,0 +1,20 @@
+/**
+ * Tommy, Zach, Dennis.
+ * Wk5
+ * 
+ * Description:
+ * zombie is an abstract class representing any zombie type
+ * in the Zombie War simulation. All zombies share basic combat
+ * attributes from Character.
+ */
+public abstract class Zombie extends Character {
+
+    public Zombie(int health, int attack) {
+        super(health, attack);
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName();
+    }
+}
