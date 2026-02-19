4b5a8692c0c02ded272d6da9e47f115a973349aa
Zach <christiz@csp.edu>
2026-02-19 16:23:08 -0600

    Fix that uses random survivor/zombie generation and updates Main

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 /**
- * Creates a zombie was simulation, runs the battle, and prints a summary report.
+ * Creates a zombie war simulation, runs the battle, and prints a summary report.
  */
 public class Main {
 
     public static void main(String[] args) {
 
         /**
-         * Creates a simulation with a specified number of survivors and zombies.
+         * Creates a simulation that randomly generates survivors and zombies.
          */
-        ZombieWarSimulation simulation = new ZombieWarSimulation(17, 8);
+        ZombieWarSimulation simulation = new ZombieWarSimulation();
 
         // Run the battle until one side is eliminated
         simulation.run();
